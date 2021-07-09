77c8cf357b291e41c6728571e0caa09ba625d88c
systemplado <systemplado@gmail.com>
2021-07-10 01:32:59 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import morgan from 'morgan'
+import mongoose from 'mongoose'
+import { config } from 'dotenv'
+
+import api from './lib/router'
+
+config()
+const app = express()
+const PORT = process.env.PORT || 3000
+
+mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+  .then(res => app.listen(PORT, console.log(`${process.env.APP_NAME} is listening at ${PORT}`)))
+  .catch(err => console.error(err))
+
+app.use(morgan('dev'))
+app.use(express.urlencoded({ extended: true }))
+app.use('/api', api)
