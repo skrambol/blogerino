034363174dda623872973b69690c649f3cb075a1
systemplado <systemplado@gmail.com>
2021-07-12 10:10:30 +0800

    better error handling and fixed filenames

--- index.js
+++ index.js
@@ -8,11 +8,17 @@ import routes from './lib/routes'
 config()
 const app = express()
 const PORT = process.env.PORT || 3000
+const mongo_opts = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+}
 
-mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.DB_URL, mongo_opts)
   .then(res => app.listen(PORT, console.log(`${process.env.APP_NAME} is listening at ${PORT}`)))
   .catch(err => console.error(err))
 
 app.use(morgan('dev'))
+app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use('/api', routes)
