60de50649f5ed92883a1e9e1a7a7261f2fc510b8
Christopher Templado <systemplado@users.noreply.github.com>
2021-07-12 10:16:27 +0800

    Refactor (#1)
    * updated npm version
    
    * refactored router
    
    * better error handling and fixed filenames

--- index.js
+++ index.js
@@ -3,16 +3,22 @@ import morgan from 'morgan'
 import mongoose from 'mongoose'
 import { config } from 'dotenv'
 
-import api from './lib/router'
+import routes from './lib/routes'
 
 config()
 const app = express()
 const PORT = process.env.PORT || 3000
+const mongo_opts = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+}
 
-mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.DB_URL, mongo_opts)
   .then(res => app.listen(PORT, console.log(`${process.env.APP_NAME} is listening at ${PORT}`)))
   .catch(err => console.error(err))
 
 app.use(morgan('dev'))
+app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use('/api', api)
+app.use('/api', routes)
