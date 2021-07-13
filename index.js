007af8f51941702aa1e6400b7fc31f661e5047cb
systemplado <systemplado@gmail.com>
2021-07-13 19:15:17 +0800

    - added jwt auth controller/routes - removed unused functions in user controller

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+import { config } from 'dotenv'
 import express from 'express'
+import cookieParser from 'cookie-parser'
 import morgan from 'morgan'
 import mongoose from 'mongoose'
-import { config } from 'dotenv'
 
 import routes from './lib/routes'
 
@@ -21,4 +22,5 @@ mongoose.connect(process.env.DB_URL, mongo_opts)
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
 app.use('/api', routes)
