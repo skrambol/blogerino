c2ac96b61b6162e600745adbae861a0dc01d0006
Christopher Templado <systemplado@users.noreply.github.com>
2021-07-14 20:26:25 +0800

    Feature/test (#4)
    * test for /api/auth routes
    - added tests for signup, login, logout
    - corrected username and password regex in User
    - created app.js that functions as the backend server
    
    * added test cases for auth, user, post routes
    - updated mongo_opts to avoid warnings
    - refactored errors.builder to Error obj
    - refactored most controllers and models to pass all tests

--- index.js
+++ index.js
@@ -1,26 +1,12 @@
 import { config } from 'dotenv'
-import express from 'express'
-import cookieParser from 'cookie-parser'
-import morgan from 'morgan'
-import mongoose from 'mongoose'
 
-import routes from './lib/routes'
+import makeApp from './lib/app'
 
 config()
-const app = express()
-const PORT = process.env.PORT || 3000
-const mongo_opts = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false
-}
-
-mongoose.connect(process.env.DB_URL, mongo_opts)
-  .then(res => app.listen(PORT, console.log(`${process.env.APP_NAME} is listening at ${PORT}`)))
+makeApp(process.env.DB_URL)
+  .then(app => {
+    app.listen(process.env.PORT, () => {
+      console.log(`${process.env.APP_NAME} is listening at port ${process.env.PORT}`)
+    })
+  })
   .catch(err => console.error(err))
-
-app.use(morgan('dev'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(cookieParser())
-app.use('/api', routes)
